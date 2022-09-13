cc0be3cbdfbb184c4668a3dd18fd9b401c809a7b
mariuslohoho <83119561+mariuslohoho@users.noreply.github.com>
2022-09-13 18:28:44 +0100

    python code

---
+++ variables.py
@@ -0,0 +1,46 @@
+
+
+# Made By Marius Lo
+
+# Variables
+
+# Variables is a value that can be changed
+# Usually for storing a value
+
+# (a name for the variable) = (a value)
+
+#Basic Number Variables
+
+
+
+print("=================")
+
+x = 10  # Sets x to 10
+y = 24  # Sets y to 24
+
+print(x) # prints out x, which is 10
+print(y) # prints out y, which is 24
+
+del x, y # Delets the variable
+
+print("=================")
+
+
+# =======================
+#   How can we use it?
+# =======================
+
+# Using for storing a person's info
+# Ex. Job Interview
+
+
+name = "Mario" # Sets the name as "Mario"
+age = 30 # Sets the age as "30"
+experince = "1 year" # Sets the experince to "1 year"
+want_to_be = "Pilot" # Sets the want_to_be to "Pilot"
+
+print("Hi, my name is", name,". I am", age,"years old. I want to be a", want_to_be,". I have", experince,"of work experince")
+
+print("=================")
+
+# =======================
