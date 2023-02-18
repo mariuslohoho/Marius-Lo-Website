a751a5f34208eef60287c3a8005c7c2d15e77860
mariuslohoho <83119561+mariuslohoho@users.noreply.github.com>
2023-02-18 23:49:33 +0000

    who doesn't like quiz?

---
+++ The questioning guy/javascript.js
@@ -0,0 +1,49 @@
+
+
+
+function clears(){
+    document.getElementById("inputBox").value = ""
+}
+
+
+
+
+const Questions = ["Q1","Q2","Q3","Q4","Q5"]
+const Answers   = ["A1","A2","A3","A4","A5"]
+
+const inputBox = document.getElementById("inputBox")
+
+let i = 0
+let score = 0
+
+
+document.getElementById("legend").innerHTML = "Question: " + Questions[0]
+
+
+function submit(){
+    
+    if (inputBox.value == Answers[i]){
+        score += 1
+        openPopup()
+    }   else{
+        openPopup2()
+        document.getElementById("des").innerHTML = "You've answer the wrong answer! <br> Correct Answer is: " + Answers[i]
+    }
+    
+    setTimeout(() => {
+        inputBox.value = ""
+    }, 1000);
+    
+    setTimeout(() => {
+        document.getElementById("legend").innerHTML = "Question: " + Questions[i]
+    }, 1000);
+
+    
+    i += 1
+
+    if (i == Answers.length){
+        document.getElementById("desc").innerHTML = "You've completed the quiz! <br> Your score is " + score+ "/" + Answers.length
+        openPopup3()
+    }
+
+}
---
+++ The questioning guy/popup.js
@@ -0,0 +1,36 @@
+
+// CORRECT POPUP
+
+function openPopup(){
+    document.getElementsByClassName("popup")[0].classList.add("active");
+    document.getElementById("overlay").classList.add("active");
+}
+
+function closePopup(){
+    document.getElementsByClassName("popup")[0].classList.remove("active");
+    document.getElementById("overlay").classList.remove("active");
+}
+
+// WRONG POPUP
+
+function openPopup2(){
+    document.getElementsByClassName("popup2")[0].classList.add("active");
+    document.getElementById("overlay").classList.add("active");
+}
+
+function closePopup2(){
+    document.getElementsByClassName("popup2")[0].classList.remove("active");
+    document.getElementById("overlay").classList.remove("active");
+}
+
+// END POPUP
+
+function openPopup3(){
+    document.getElementsByClassName("popup3")[0].classList.add("active");
+    document.getElementById("overlay").classList.add("active");
+}
+
+function closePopup3(){
+    document.getElementsByClassName("popup3")[0].classList.remove("active");
+    document.getElementById("overlay").classList.remove("active");
+}
